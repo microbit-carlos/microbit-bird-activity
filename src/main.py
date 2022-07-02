051bdb32d4648f021e08cec2e6834dcf02d51717
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2022-07-02 13:21:02 +0100

    Add temp SoundEffect module until it's implemented in MicroPython.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
-# Imports go at the top
 from microbit import *
 import bird
-import music
+import audio_soundeffect as audio
 
-def pitch_sweep():
-    for i in range(4000,5000,50):
-        music.pitch(i)
-        sleep(10)
-    music.pitch(0)
-
-# Code in a 'while True:' loop repeats forever
 while True:
     action = bird.process_world()
     # process world could return any of:
