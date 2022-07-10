4626bbf5b30ed737c874fecfdd8e5f35a9bd8723
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2022-07-10 19:01:52 +0100

    Bird says hello on startup and waits until it hears it back.

--- src/bird/bird.py
+++ src/bird/bird.py
@@ -1,22 +1,99 @@
 # microbit-module: bird@0.0.1
 import radio as __radio
 import microbit as __mb
+import machine as __machine
+import struct as __struct
+import random as __rand
 
-__radio.config(group=7)
-__radio.on()
-__radio.send('hello')
+BIRD_NAME = None
+MSG_DELIMITER = "->"
+RADIO_GROUP_HELLO = 1
+RADIO_GROUP_BIRDS = 17
 
-def __wait_for_start():
-    while True:
+# Defined radio messages to react to
+_events = ['hello', 'cat', 'hawk', 'food', 'dawn', 'dusk']
+_states = ["chill", "hungry", "angry"]
+
+# Callbacks for the react decorator
+__event_callbacks = {}
+
+def react(function, radio_event):
+    """Decorator to register function callbacks for radio messages."""
+    if radio_event in _events:
+        __event_callbacks[radio_event] = function
+    else:
+        raise Exception("Invalid message registered via @react decorator.")
+    return function
+
+
+def __hi_everyone_i_am_here():
+    """Say hello to the world until you hear hello back to you.
+    The micro:bit that transmits radio events keeps a list of active birds,
+    so this is a way to register your bird is active.
+    """
+    print("Hello, my name is {}. Can anybody hear me...?".format(BIRD_NAME))
+    __radio.config(group=RADIO_GROUP_HELLO, power=7)
+    __radio.on()
+    msg_out = "hello{}{}".format(MSG_DELIMITER, BIRD_NAME)
+    timeout = __mb.running_time() + (60 * 1000)
+    while __mb.running_time() < timeout:
+        try:
+            __radio.send(msg_out)
+            __mb.sleep(__rand.randint(100,200))
+            radio_received = __radio.receive()
+        except:
+            pass
+        else:
+            if radio_received == msg_out:
+                # So glad somebody finally said hello back, it was getting kind of rude
+                # I can go on my business now
+                print("🎉 Somebody said hello back, this is such a friendly neighbourhood!")
+                break
+    else:
+        # I've been waiting too long to be acknowledged
+        print("Warning! Bird could not hear any hello back.")
+    # Move to the radio group with all the birds
+    __radio.config(group=RADIO_GROUP_BIRDS, power=0)
+
+
+def __friendly_name():
+    length, letters = 5, 5
+    codebook = [
+        ['z', 'v', 'g', 'p', 't'],
+        ['u', 'o', 'i', 'e', 'a'],
+        ['z', 'v', 'g', 'p', 't'],
+        ['u', 'o', 'i', 'e', 'a'],
+        ['z', 'v', 'g', 'p', 't']
+    ]
+    name = []
+    # Derive our name from the unique ID
+    _, n = __struct.unpack("II", __machine.unique_id())
+    ld = 1;
+    d = letters;
+    for i in range(0, length):
+        h = (n % d) // ld;
+        n -= h;
+        d *= letters;
+        ld *= letters;
+        name.insert(0, codebook[i][h]);
+    return "".join(name);
+
+
+@__mb.run_every(s=2)
+def __check_radio_msgs():
+    try:
         message = __radio.receive()
-        if message == "start":
-            return
-            
-_radio_messages = ['hello', 'cat', 'hawk', 'food', 'dawn', 'dusk']
+    except:
+        print("There was an error reading radio")
+        return
+    try:
+        #process_world(message)
+        pass
+    except:
+         print("There was an error processing the world")
 
-normal = "hello"
 
-def process_world():
+def process_world(message):
     '''
     This is the bird's brain, processing the world around.
     The bird knows several 'actions' that can happen as a result of thinking
@@ -44,18 +121,18 @@ def process_world():
         # food: time for dinner
         # dawn: time to wake up
         # dusk: time to go to sleep
-    message = __radio.receive()
     if message:
         if message == "stop":
             #sleep!
             #while we don't have sleep, just loop waiting for the start command
-            __wait_for_start()
+            #__wait_for_start()
+            pass
         elif (message == "hawk"  or message == "cat"):
             #impose a small delay before warning others
             __mb.sleep(200)
             return message
         else:
-            if message in _radio_messages:
+            if message in _events:
                 return message
     # Check for a loud noise
     if __mb.microphone.current_event() == __mb.SoundEvent.LOUD:
@@ -63,10 +140,23 @@ def process_world():
     # Check for motion
     if __mb.accelerometer.was_gesture('shake'):
         return 'motion'
+    __mb.sleep(10)   # Ensure run_every has a chance to run
     return "chill"
+
 
 def warn_about_hawk():
     __radio.send("hawk")
 
+
 def warn_about_cat():
     __radio.send("cat")
+
+
+def __init():
+    global BIRD_NAME
+    BIRD_NAME = __friendly_name()
+    __hi_everyone_i_am_here()
+
+
+# We run some init code on import
+__init()
