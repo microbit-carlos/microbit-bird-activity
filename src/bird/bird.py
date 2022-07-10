3782499a477957e848d04ff28d814313993348f4
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2022-07-10 22:43:00 +0100

    bird: Start processing the world (radio events first, start with 'hello').

--- src/bird/bird.py
+++ src/bird/bird.py
@@ -1,4 +1,22 @@
 # microbit-module: bird@0.0.1
+# Welcome to the bird brain, processing the world around it!
+# The bird notices several 'events' that can happen in their environment
+# and can react to them.
+# These events are processed via the @react("event_name") decorator in main.py
+#     - hello: new bird has arrived in the flock
+#     - cat : predator below
+#     - hawk: predator above
+#     - dawn: time to wake up
+#     - dusk: time to go to sleep
+#     - ...extend your bird by editing the bird.py file!
+#
+# The bird can also
+#     - food: time for dinner
+#     - squawk: something loud happened
+#     - motion: some kind of movement happened
+#     - chill: nothing is happening, rest
+#
+# Modules imported with __ to keep them out of the autocompletion in main.py
 import radio as __radio
 import microbit as __mb
 import machine as __machine
@@ -6,24 +24,33 @@ import struct as __struct
 import random as __rand
 
 BIRD_NAME = None
-MSG_DELIMITER = "->"
-RADIO_GROUP_HELLO = 1
-RADIO_GROUP_BIRDS = 17
+__MSG_DELIMITER = "->"
+__RADIO_GROUP_HELLO = 1
+__RADIO_GROUP_BIRDS = 17
 
 # Defined radio messages to react to
-_events = ['hello', 'cat', 'hawk', 'food', 'dawn', 'dusk']
+_events = ["hello", "cat", "hawk", "food", "dawn", "dusk"]
 _states = ["chill", "hungry", "angry"]
 
 # Callbacks for the react decorator
-__event_callbacks = {}
 
-def react(function, radio_event):
-    """Decorator to register function callbacks for radio messages."""
-    if radio_event in _events:
-        __event_callbacks[radio_event] = function
-    else:
-        raise Exception("Invalid message registered via @react decorator.")
-    return function
+
+class react():
+    """Decorator to register function callbacks for events (radio messages)."""
+
+    callbacks = dict()
+
+    def __init__(self, radio_event):
+        """:param radio_event: String with the event to register."""
+        if radio_event not in _events:
+            raise Exception("Invalid message event string in @react decorator.")
+        self.radio_event = radio_event
+
+    def __call__(self, original_func):
+        self.callbacks[self.radio_event] = original_func
+        def wrappee(*args, **kwargs):
+            return original_func(*args,**kwargs)
+        return wrappee
 
 
 def __hi_everyone_i_am_here():
@@ -31,32 +58,32 @@ def __hi_everyone_i_am_here():
     The micro:bit that transmits radio events keeps a list of active birds,
     so this is a way to register your bird is active.
     """
-    print("Hello, my name is {}. Can anybody hear me...?".format(BIRD_NAME))
-    __radio.config(group=RADIO_GROUP_HELLO, power=7)
+    print("👋 Hello, my name is {}. Can anybody hear me...?".format(BIRD_NAME))
+    __radio.config(group=__RADIO_GROUP_HELLO, power=7)
     __radio.on()
-    msg_out = "hello{}{}".format(MSG_DELIMITER, BIRD_NAME)
-    timeout = __mb.running_time() + (60 * 1000)
+    msg_out = "hello{}{}".format(__MSG_DELIMITER, BIRD_NAME)
+    timeout = __mb.running_time() + (30 * 1000)
     while __mb.running_time() < timeout:
         try:
             __radio.send(msg_out)
-            __mb.sleep(__rand.randint(100,200))
+            __mb.sleep(__rand.randint(100,300))
             radio_received = __radio.receive()
         except:
             pass
         else:
             if radio_received == msg_out:
-                # So glad somebody finally said hello back, it was getting kind of rude
-                # I can go on my business now
+                # So glad somebody said hello back, I can go on with my business now
                 print("🎉 Somebody said hello back, this is such a friendly neighbourhood!")
                 break
     else:
         # I've been waiting too long to be acknowledged
         print("Warning! Bird could not hear any hello back.")
     # Move to the radio group with all the birds
-    __radio.config(group=RADIO_GROUP_BIRDS, power=0)
+    __radio.config(group=__RADIO_GROUP_BIRDS, power=1)
 
 
 def __friendly_name():
+    """Returns a string with a friendly name based on MCU Unique ID."""
     length, letters = 5, 5
     codebook = [
         ['z', 'v', 'g', 'p', 't'],
@@ -82,34 +109,38 @@ def __friendly_name():
 @__mb.run_every(s=2)
 def __check_radio_msgs():
     try:
-        message = __radio.receive()
+        msg = __radio.receive()
     except:
-        print("There was an error reading radio")
+        print("Warning: There was an unexpected error reading radio.")
         return
     try:
-        #process_world(message)
-        pass
-    except:
-         print("There was an error processing the world")
+        if msg:
+            __process_world(msg)
+    except Exception as e:
+        print("Warning: There was an error processing the world")
+        print(e)
 
 
-def process_world(message):
-    '''
-    This is the bird's brain, processing the world around.
-    The bird knows several 'actions' that can happen as a result of thinking
-     - hello: new bird has arrived in the flock
-     - cat : predator below
-     - hawk: predator above
-     - food: time for dinner
-     - dawn: time to wake up
-     - dusk: time to go to sleep
-     - squawk: something loud happened
-     - motion: some kind of movement happened
-     - chill: nothing is happening, rest
-     - ...extend your bird by editing the bird.py file!
-    
-    :return: action (str): one of the possible actions for the bird
-    '''
+def __process_world(message):
+    # First check if this message is for us
+    global BIRD_NAME
+    msg_split = message.split(__MSG_DELIMITER)
+    if len(msg_split) != 2:
+        print("👂❗️ Heard something weird! Not quite sure what it means:".format(message))
+        return
+    if msg_split[1] not in [BIRD_NAME, "all"]:
+        # This message is for a different bird, we can ignore it
+        return
+
+    # Okay, the message is for us, let's figure out what to do with it!
+    print("👂 I've heard something: {}".format(message))
+    # First we run any code specific for the message
+    if msg_split[0] == "hello":
+        __hi_everyone_i_am_here()
+    # Then we run the user callback
+    if msg_split[0] in react.callbacks:
+        react.callbacks[msg_split[0]]()
+    return
     # Check for radio signals:
     # for us/under the hood: 
         # stop
@@ -134,6 +165,9 @@ def process_world(message):
         else:
             if message in _events:
                 return message
+
+
+def current_state():
     # Check for a loud noise
     if __mb.microphone.current_event() == __mb.SoundEvent.LOUD:
         return 'squawk'
@@ -145,11 +179,11 @@ def process_world(message):
 
 
 def warn_about_hawk():
-    __radio.send("hawk")
+    __radio.send("hawk->all")
 
 
 def warn_about_cat():
-    __radio.send("cat")
+    __radio.send("cat->all")
 
 
 def __init():
