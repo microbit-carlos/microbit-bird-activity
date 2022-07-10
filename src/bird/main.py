b95fa45ba985d16c36c14ec1ab39e0365480f735
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2022-07-10 23:54:00 +0100

    bird: Update main.py script to have decorated events or if/else bird status.

--- src/bird/main.py
+++ src/bird/main.py
@@ -1,27 +1,38 @@
+# Welcome!
+# This is the main script for your bird. Some of the functionality is in the
+# bird.py file, feel free to have a peek if you like, but there is no need yet.
 from microbit import *
 import bird
 import audio_soundeffect as audio
 
+# Your bird will react to events in their environment via decorators like the
+# one below this comment. You can modify this example and create new ones for
+# all the other events:
+# TODO: copy the final list here
+
+BIG_HEART = True
+
+@bird.react("hello")
+def hello():
+    """Somebody is saying hi, how lovely!"""
+    # Toggle the display between small and large heart
+    global BIG_HEART
+    display.show(Image.HEART if BIG_HEART else Image.HEART_SMALL)
+    BIG_HEART = not BIG_HEART
+
+@bird.react("cat")
+def cat():
+    """ What is that?? I think I just saw a cat!
+    Remember to warn the rest of the birds!"""
+    pass
+
+
+# But first, let's start reacting to 
 while True:
-    action = bird.process_world()
-    # process world could return any of:
-    if action == "chill":
-        display.show(Image.HEART)    
-    elif action == "hello":
-        pass 
-    elif action == "cat":
-        # warn the 
-        bird.warn_about_cat() 
-    elif action == "hawk":
-        # Warn the other birds!
-        bird.warn_about_hawk() 
-    elif action == "food":
-        pass
-    elif action == "dawn":
-        pass
-    elif action == "dusk":
-        pass
-    elif action == "motion":
-        pass
-    elif action == "squawk":
-        pass        
+    # Look inside yourself and listen, how is your bird feeling?
+    bird_state = bird.current_state()
+    if bird_state == "chill":
+        display.show(Image.HAPPY)
+    elif bird_state == "angry":
+        display.show(Image.ANGRY)
+    sleep(100)
